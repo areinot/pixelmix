3ae18d9914f613cc63e87ecb377d06d9c545d532
Andres <andres.reinot@gmail.com>
2017-09-29 21:43:41 -0500

    no message

---
+++ pixelmix.js
@@ -0,0 +1,153 @@
+
+class PixelMix { //@@@ extends HTMLElement {
+	constructor(element) {
+		//@@@ super(); 
+		
+		//VARS
+
+		//Note: init is optionally separate from the constructor to better time when DOM creation and loading callbacks happen.
+		//For example an oncomplete callback may want to reference "this" but constructors are run in a limbo state where "this"
+		//is not yet defined.
+		this.element = null;
+		this.slider = null;
+		this.images = [];
+		this.imageA = 0;		//lesser image index
+		this.imageB = 0;		//next image index
+		this.imageT = 0.0;		//interpolant between the first image and the last
+		this.blendT = 0.0;		//interpolant between image A and image B
+
+		//DOM
+
+		if(element) {
+			this.element = element;
+			this.element.style = element.style || {};
+			this.images = element.getElementsByTagName("img") || [];
+
+			//this.element.style.overflow = "hidden";
+
+			//TODO: attributes
+			// var x = element.getAttribute("canvasX");
+			// var width = Number.parseInt(element.getAttribute("width"));
+		} else {
+			console.error("Pixel-Mix Error: No parent DOM element found.");
+			return;
+		}
+
+		if(this.images.length > 0) {
+			//make sure the div or whatever can fit our images
+			for(var i=0; i<this.images.length; i++) {
+				var img = this.images[i];
+				img.style = img.style || {};				
+				img.style.opacity = 1.0;
+				img.style.visibility = "hidden";
+				if(i == 0) {
+					img.style.position = "relative";
+				} else {
+					img.style.position = "absolute";
+				}
+				this._fixImageSize(img);
+				this._noselect(img.style);
+			}			
+		}
+		else
+		{
+			console.error("Pixel-Mix Error: Parent DOM element needs at least one image element.");
+			return;
+		}	
+		
+		//EVENTS
+
+		this.element.addEventListener("click", function(ev) {			
+			var t = this.imageT + 0.05;
+			if(t > 1.0) t -= 1.0;
+			this.pickImageByFraction(t);
+		}.bind(this));		
+
+		//TODO: throttle this
+		window.addEventListener("resize", function(ev) {
+			return;
+			if(this.images && this.images.length) {
+				this._fixImageSize(this.images[this.imageA]);
+				this._fixImageSize(this.images[this.imageB]);
+			}
+		}.bind(this));
+
+		//SLIDER
+
+		if( this.element.id !== undefined && this.element.id !== null) {
+			this.slider = document.getElementById(this.element.id + "-slider");
+			this.slider.min = 0;
+			this.slider.max = 100;
+			this.slider.value = 50;		
+			this.slider.addEventListener("input", function(ev) {				
+				this.pickImageByFraction(this.slider.value * 0.01);
+			}.bind(this));
+		}
+
+		this.pickImageByFraction(0.5);	
+	}
+
+	pickImageByFraction(t) {
+		this.images[this.imageA].style.visibility =
+		this.images[this.imageB].style.visibility = "hidden";
+
+		t = Math.max(0.0, Math.min(t, 1.0));
+		var span = this.images.length-1;
+		this.imageA = Math.floor(t * span);
+		this.imageB = Math.min(this.imageA + 1, span + 1);
+		this.blendT = (t * span) - this.imageA;
+
+		if(this.imageB == span + 1) {
+			this.blendT = 1.0; //very last step needs to end at A:0, B:1
+			this.imageB = span;
+		}
+		
+		var styleA = this.images[this.imageA].style;
+		var styleB = this.images[this.imageB].style;
+
+		styleA.visibility =
+		styleB.visibility = "visible";
+		
+		styleA.opacity = 1.0;
+		styleB.opacity = this.blendT;
+
+		this._fixImageSize(this.images[this.imageA]);
+		this._fixImageSize(this.images[this.imageB]);
+		
+		this.imageT = t;
+	}
+
+	static initDOM() {
+		var mixers = [];
+		Array.prototype.filter.call( document.getElementsByTagName("pixel-mix"), function(el) {
+			this.push(new PixelMix(el));
+		}.bind(mixers));
+
+		Array.prototype.filter.call( document.getElementsByClassName("pixel-mix"), function(el) {
+			this.push(new PixelMix(el));
+		}.bind(mixers));
+
+		return mixers;
+	}
+
+	//helper nonsense
+
+	_noselect(style) {
+		style["-webkit-user-select"] = "none";  // Chrome, Safari
+		style["-moz-user-select"] = "none";		//Firefox
+		style["-ms-user-select"] = "none"; 		// IE 10+
+		style["user-select"] = "none";
+	}
+
+	_fixImageSize(img) {
+		if(img !== this.images[0]) {
+			var rect = this.images[0].getBoundingClientRect();
+			console.log(rect);
+			img.style.left = rect.left;
+			img.style.top =  rect.top;
+			img.style.width =  Math.abs(rect.right - rect.left);
+			img.style.height = Math.abs(rect.top - rect.bottom);
+		}
+	}
+}
+
